d7ec35ba0ee0f6f20f6432276a90b41fae975cd5
Diniso Gwabeni <dinisogwabeni@gmail.com>
2026-07-18 11:23:30 +0200

    fixed coding error

--- backend/app/gamification.py
+++ backend/app/gamification.py
@@ -22,9 +22,9 @@ def _award(db: Session, user:models.User, code:str, unlocked: list):
     achh = db.query(models.Achievement).filter_by(code=code).first()
     if not achh:
         return
-    already = db.query(models.UserAchievement).filter_by(user_id=user_i, achievement_id=achh.id).first()
+    already = db.query(models.UserAchievement).filter_by(user_id=user.id, achievement_id=achh.id).first()
     if not already:
-        db.add(models.UserAchievement(user_id=user_i, achievement_id=achh.id))
+        db.add(models.UserAchievement(user_id=user.id, achievement_id=achh.id))
         unlocked.append(achh.title)
 
 def update_streak(user: models.User):
